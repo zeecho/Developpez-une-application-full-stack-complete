eb1a913e69665720a0355e5d7afdf74979798d28
zecho <zeecho@users.noreply.github.com>
2024-04-15 18:06:43 +0200

    Upgrade angular-material

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
