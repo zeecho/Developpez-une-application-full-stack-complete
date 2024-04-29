71decc4fbea35d3b0a2764e3560d0715bd00aae8
zecho <zeecho@users.noreply.github.com>
2024-04-29 12:28:22 +0200

    Implement burger menu and other small css improvements

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { AuthService } from './features/auth/services/auth.service';
-import { Router } from '@angular/router';
+import { NavigationStart, Router } from '@angular/router';
 import { SessionService } from './services/session.service';
 import { Observable } from 'rxjs';
 
@@ -10,10 +10,18 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {  
+  isMenuOpen: boolean = false;
+
   constructor(
     private authService: AuthService,
     private router: Router,
     private sessionService: SessionService) {
+      this.router.events.subscribe(event => {
+        if (event instanceof NavigationStart) {
+          // Close menu when navigating to another page (when burger menu is activated)
+          this.closeMenu();
+        }
+      });
   }
 
   public $isLogged(): Observable<boolean> {
@@ -22,6 +30,26 @@ export class AppComponent {
 
   isHomeRoute(): boolean {
     return this.router.url === '/';
+  }
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  closeMenu(event?: MouseEvent): void {
+    if (!event) {
+      // Close menu when navigating to another page (when burger menu is activated)
+      this.isMenuOpen = false;
+    } else {
+      // Close menu when clicking outside the menu (when burger menu is activated)
+      const targetElement = event.target as HTMLElement;
+      const menuElement = document.querySelector('.mobile-menu') as HTMLElement;
+      const burgerMenuIcon = document.querySelector('.burger-menu') as HTMLElement;
+      if (menuElement && !menuElement.contains(targetElement) && !burgerMenuIcon.contains(targetElement)) {
+        this.isMenuOpen = false;
+      }
+    }
   }
 
   title = 'MDD';
